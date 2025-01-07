393cbf2844c7a1bdb6dc8f613907c2af259cfe64
Jayme Desrosiers <40217128+fatal-zio@users.noreply.github.com>
2025-01-07 17:50:57 -0600

    Added logic for Quiz completion

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,11 +1,13 @@
 import { useState } from "react";
 
-import QUESTIONS from "../questions";
+import QUESTIONS from "../questions.js";
+import quizCompleteImage from "../assets/quiz-complete.png";
 
 export default function Quiz() {
   const [userAnswers, setUserAnswers] = useState([]);
 
   const activeQuestionIndex = userAnswers.length;
+  const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
 
   function handleSelectAnswer(selectedAnswer) {
     setUserAnswers((previousUserAnswers) => {
@@ -13,14 +15,26 @@ export default function Quiz() {
     });
   }
 
+  if (quizIsComplete) {
+    return (
+      <div id="summary">
+        <img src={quizCompleteImage} alt="Trophy icon" />
+        <h2>Quiz Completed!</h2>
+      </div>
+    );
+  }
+
+  const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
+  shuffledAnswers.sort(() => Math.random() - 0.5);
+
   return (
     <div id="quiz">
       <div id="question">
         <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
         <ul id="answers">
-          {QUESTIONS[activeQuestionIndex].answers.map((answer) => (
+          {shuffledAnswers.map((answer) => (
             <li key={answer} className="answer">
-              <button onclick={() => handleSelectAnswer(answer)}>
+              <button onClick={() => handleSelectAnswer(answer)}>
                 {answer}
               </button>
             </li>
