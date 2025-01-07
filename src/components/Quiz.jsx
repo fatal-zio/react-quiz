5d54341ff9fee818a87393d88c39fd03679aee55
Jayme Desrosiers <40217128+fatal-zio@users.noreply.github.com>
2025-01-07 16:35:12 -0600

    Initial layout of Quiz component

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,7 +1,32 @@
 import { useState } from "react";
 
+import QUESTIONS from "../questions";
+
 export default function Quiz() {
-  const [activeQuestionIndex, setActiveQuestionIndex] = useState(0);
   const [userAnswers, setUserAnswers] = useState([]);
-  return <p>Currently active Question</p>;
+
+  const activeQuestionIndex = userAnswers.length;
+
+  function handleSelectAnswer(selectedAnswer) {
+    setUserAnswers((previousUserAnswers) => {
+      return [...previousUserAnswers, selectedAnswer];
+    });
+  }
+
+  return (
+    <div id="quiz">
+      <div id="question">
+        <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
+        <ul id="answers">
+          {QUESTIONS[activeQuestionIndex].answers.map((answer) => (
+            <li key={answer} className="answer">
+              <button onclick={() => handleSelectAnswer(answer)}>
+                {answer}
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 }
